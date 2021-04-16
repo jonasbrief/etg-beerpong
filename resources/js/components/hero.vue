bc7f2d623daf4757e9be831f3889725ceb9e2a07
Jonas Brief <jonas@Workstation.localdomain>
2021-04-16 17:17:06 +0200

    Update hero

--- resources/js/components/hero.vue
+++ resources/js/components/hero.vue
@@ -6,13 +6,19 @@
     </p>
     <p class="subtitle">
       Viel Spaß und Trinkfestigkeit beim digital-geselligen genießen diverser kühler Blonder. <br> 
-      Um die Funktionen der Wesbite zu verstehen klickt auf <i>Manuel</i>, für das offizielle Regelwerk der European Series of Beer Pong klickt auf <i>Regelwerk</i> <br>
+      Für das offizielle Regelwerk der European Series of Beer Pong klickt auf <i>Regelwerk</i> <br>
       <strong> Gut kick! </strong>
       
     </p>
     <p class="content">
        <button class="button is-light is-link" type="button"> Manuel </button>
        <a href="https://cdn.shopify.com/s/files/1/0001/9064/4262/files/beerpong-turnierregeln.pdf?v=1603379144" target="blank"> <button class="button button is-light is-link"> Regelwerk </button>  </a>
+    </p>
+
+    <p class="subtitle">
+      Erstellt zuerst euer Team über den Create Team Knop oben rechts. Dann könnt ihr euer Team und das gegnereische Team zu einem Raum hinzufügen.
+      Achtung, nur ein Team fügt beide Teams zum Raum hinzu und startet das Spiel. <br>
+      Der aktuelle Stand des Raumens wird durch Neuladen der Seite geladen.
     </p>
   </div>
 </section>
